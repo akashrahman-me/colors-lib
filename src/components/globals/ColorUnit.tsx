a9fafa7992a1be96ea572f214e59855deedaeef7
Akash Rahman <akashrahman.me@gmail.com>
2024-09-27 18:31:34 +0600

    desktop version completed

--- src/components/globals/ColorUnit.tsx
+++ src/components/globals/ColorUnit.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 "use client";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import { useEffect, useState } from "react";
+import { TabPanel, Tabs } from "react-tabs";
+import { useEffect, useRef, useState } from "react";
 import HSBColorPicker from "@/components/globals/HSBColorPicker";
 import RGBColorPicker from "./RGBColorPicker";
 import HSLColorPicker from "./HSLColorPicker";
 import ColorPicker from "./ColorPicker";
+import { motion } from "framer-motion";
 
 const colorOptions = ["Picker", "RGB", "HSL", "HSB"];
 
@@ -15,9 +17,18 @@ interface ColorUnitProps {
 }
 
 function ColorUnit({ label, setColor, color }: ColorUnitProps) {
-   // console.log("Color Unit");
-
    const [value, setValue] = useState(color);
+   const [tabIndex, setTabIndex] = useState(2); // Default tab index to 0
+
+   useEffect(() => {
+      // Load saved tab index from localStorage, if it exists
+      const savedTabIndex = localStorage.getItem(`${label}-selected-tab`);
+
+      if (savedTabIndex && Number(savedTabIndex) !== tabIndex) {
+         setTabIndex(Number(savedTabIndex));
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
    useEffect(() => {
       if (value !== color) {
@@ -30,40 +41,124 @@ function ColorUnit({ label, setColor, color }: ColorUnitProps) {
       setColor(color);
    };
 
+   const tabsButtonRef = useRef<Array<HTMLElement | null>>([]);
+   const tabsButtonWidth = useRef<Array<number>>([]);
+   const [paper, setPaper] = useState({
+      x: 0,
+      width: 0,
+   });
+
+   useEffect(() => {
+      tabsButtonRef.current.forEach((button, tabIndex) => {
+         const width = button?.clientWidth || 0;
+         tabsButtonWidth.current[tabIndex] = width;
+      });
+   }, [tabsButtonRef]);
+
+   useEffect(() => {
+      localStorage.setItem(`${label}-selected-tab`, String(tabIndex));
+
+      let x: number = 0;
+      if (
+         tabsButtonWidth.current.length !== 0 &&
+         tabsButtonWidth.current.slice(0, tabIndex).length !== 0
+      ) {
+         x = tabsButtonWidth.current.slice(0, tabIndex).reduce((a, b) => a + b);
+      }
+      const width = tabsButtonWidth.current[tabIndex] || 0;
+      setPaper({ x, width });
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [tabIndex]);
+
+   const [copied, setCopied] = useState(false);
+
+   const copyToClipboard = () => {
+      const colorValue = value;
+      navigator.clipboard.writeText(colorValue).then(() => {
+         setCopied(true);
+         setTimeout(() => setCopied(false), 2000); // Reset copied status after 2 seconds
+      });
+   };
+
    return (
       <div className="bg-white pt-6 px-6 pb-12 flex gap-6 flex-col">
          <h4 className="text-xl leading-normal">{label}</h4>
          <div className="flex gap-6">
-            <input
-               value={value}
-               onChange={(e) => setValue(e.currentTarget.value)}
-               onBlur={(e) => submitColor(e.currentTarget.value)}
-               className="bg-primary-700/10 uppercase py-2.5 px-4 border-b border-primary-700 text-base leading-normal min-w-0 w-full max-w-[150px] text-center"
+            <div className="relative">
+               <div className="text-base leading-normal uppercase absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-1/2">
+                  <motion.span
+                     className="block"
+                     initial={{ opacity: 0 }}
+                     animate={copied ? { opacity: [1, 0], y: [0, -56] } : {}}
+                     transition={{ duration: 0.5 }}
+                  >
+                     {value}
+                  </motion.span>
+               </div>
+               <motion.input
+                  transition={{ duration: 0.5 }}
+                  animate={
+                     copied
+                        ? {
+                             x: [
+                                10, 0, -10, 8, 0, -8, 6, 0, -6, 4, 0, -4, 2, 0, -2, 1, 0,
+                                -1,
+                             ],
+                          }
+                        : {}
+                  }
+                  value={value}
+                  onChange={(e) => setValue(e.currentTarget.value)}
+                  onBlur={(e) => submitColor(e.currentTarget.value)}
+                  className="bg-primary-700/10 uppercase py-2.5 px-4 border-b border-primary-700 text-base leading-normal min-w-0 w-full max-w-[150px] text-center"
+               />
+            </div>
+            <div
+               onClick={copyToClipboard}
+               className="py-2.5 px-5 flex-1"
+               role="button"
+               style={{ backgroundColor: color }}
             />
-            <div className="py-2.5 px-5 flex-1" style={{ backgroundColor: color }} />
          </div>
-         <Tabs className="color-adjust-tab flex flex-col gap-6">
-            <TabList className="flex gap-2.5 border-b border-primary-700">
+         <Tabs
+            className="color-adjust-tab flex flex-col gap-6"
+            selectedIndex={tabIndex}
+            onSelect={(index) => setTabIndex(index)}
+         >
+            <div className="flex border-b border-primary-700 relative z-0">
+               <motion.span
+                  animate={{
+                     x: paper.x,
+                     width: paper.width,
+                  }}
+                  transition={{ duration: 0.2 }}
+                  className=" bg-primary-700/10 min-w-20 absolute top-0 left-0 bottom-0 -z-10"
+               />
                {colorOptions.map((color, index) => (
-                  <Tab
-                     className="cursor-pointer py-1.5 px-5 flex items-center justify-center text-sm leading-normal focus:outline-0"
+                  <div
+                     ref={(e) => {
+                        tabsButtonRef.current[index] = e;
+                     }}
+                     onClick={() => setTabIndex(index)}
+                     className="cursor-pointer py-1.5 px-6 flex items-center justify-center text-sm leading-normal focus:outline-0"
                      key={index}
                   >
                      {color}
-                  </Tab>
+                  </div>
                ))}
-            </TabList>
+            </div>
             <div className="p-5 shadow-[1px_1px_2px_0px_rgba(0,_0,_0,_0.10)]">
-               <TabPanel className="">
+               <TabPanel>
                   <ColorPicker color={color} setColor={setColor} />
                </TabPanel>
-               <TabPanel className="">
+               <TabPanel>
                   <RGBColorPicker color={color} setColor={setColor} />
                </TabPanel>
-               <TabPanel className="">
+               <TabPanel>
                   <HSLColorPicker color={color} setColor={setColor} />
                </TabPanel>
-               <TabPanel className="">
+               <TabPanel>
                   <HSBColorPicker
                      color={color}
                      setColor={(value) => (setValue(value), setColor(value))}
